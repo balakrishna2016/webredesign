780fd1177593edd9c3e2b773e35d2fa3b7895d19
Muneer PP <abdulmuneer22@gmail.com>
2017-01-23 08:18:50 +0530

    started test

--- my-app/src/components/components.test.js
+++ my-app/src/components/components.test.js
@@ -1,24 +1,17 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+
+import React from 'react';
+import sinon from 'sinon';
+import { mount } from 'enzyme';
 
 import RadioButton from './wo_redux/RadioButton'
-import TickBox from './wo_redux/TickBox'
-import Toggle from './wo_redux/Toggle'
 
-it('renders RadioButton without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<RadioButton />, div);
-});
-
-
-it('renders TickBox without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<TickBox />, div);
-});
-
-
-it('renders TickBox without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Toggle />, div);
-});
-
+describe('<RadioButton />', () => {
+  it('check button class', () => {
+  
+    const wrapper = mount(<RadioButton />);
+    expect(wrapper.containsAllMatchingElements([
+      <i class="fa fa-square-o" aria-hidden="true" style="font-size: 25px; color: rgb(12, 183, 222);"></i>
+    ]).toEqual(true)
+    );
+  });
+})
